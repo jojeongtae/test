13b676d734a188904690c88d0967a398cc8879c1
jojeongtae <jeongtae9324@gmail.com>
2025-04-02 23:16:35 +0900

    List.js

--- src/List.js
+++ src/List.js
@@ -5,7 +5,7 @@ export default function List() {
     const shopList = useSelector(state => state.shop.shop) ;
     const shopLst = shopList.map((m) => 
     <Link to={`/${m.value}`} key={m.id}>{m.title}&nbsp;</Link>)
-
+//useSelector로 배열끌어온후 map 사용해서 물품 장바구니 합산 생성
 
     return (
         <>
