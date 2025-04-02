6fafdb48fcbcd39b029376133ffe31969d13b633
jojeongtae <jeongtae9324@gmail.com>
2025-04-02 23:20:32 +0900

    Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -7,7 +7,7 @@ export default function Payment() {
   cartItems.forEach(item => {
     result += item.price * item.quantity;
   });
-
+//forEach문 돌려서 배열을 처음부터 탐색후 배열안에 있는 price와 quantity를 찾고 곱한 값을 result에 더함
   return (
     <div>
       <h1>Total</h1>
